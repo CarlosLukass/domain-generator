07f1fda1204b6b798325724d4c6ce781d95f7703
CarlosLukass <carlos.lukassf@gmail.com>
2022-06-02 01:25:28 +0000

    outpud position fixed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const pronoun = ["the", "our"];
+const adj = ["great", "big"];
+const noun = [
+  "jogger",
+  "withus",
+  "agencytech",
+  "melonesgrandes",
+  "racoon",
+  "newbaby"
+];
+const domainExtensions = ["com", "es", "tech", "us", "baby", "bike"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+console.log(`All posibles domains:`);
+
+
+adj.forEach(adjElement => {
+  pronoun.forEach(pronounElement => {
+    noun.forEach(nounElement => {
+      console.log(`👉 ${pronounElement}${adjElement}${nounElement}.com`);
+      // Replace last word with domain extension
+      domainExtensions.forEach(domain => {
+        if (nounElement.toLocaleLowerCase().endsWith(domain)) {
+          const nounWithoutExtension = nounElement.slice(0, -domain.length);
+          console.log(
+            `☝️. another option: ${pronounElement}${adjElement}${nounWithoutExtension}.${domain}`
+          );
+        }
+      });
+    });
+  });
+});
